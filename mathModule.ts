1113275466685ae2237b616751cbfe36d54aea26
komal Arya <guptakomal87@gmail.com>
2017-05-23 14:00:02 +0530

    Typescript_Assignment_5.4

---
+++ mathModule.ts
@@ -0,0 +1,22 @@
+
+//B. Create a module which should have basics math operations and use them in other files.
+
+export function add(x, y) {
+    return x + y;
+}
+
+export function subtract(x, y) {
+    return x - y;
+}
+
+export function multiply(x, y) {
+    return x * y;
+}
+
+export function divide(x, y) {
+    return x / y;
+}
+
+export function square(x) {
+    return x * x;
+}
---
+++ test1.ts
@@ -0,0 +1,8 @@
+
+import {add, subtract, multiply, divide, square} from "./mathModule"
+
+console.log(`Add: ${add(10, 5)}`)
+console.log(`Subtract: ${subtract(10, 5)}`)
+console.log(`Multiply: ${multiply(10, 5)}`)
+console.log(`Divide: ${divide(10, 5)}`)
+console.log(`Square: ${square(10)}`)
